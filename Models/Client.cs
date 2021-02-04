f5e383ee3ddb028ae46157cefda39d82c01c3459
anzucel <andreazucely70@gmail.com>
2021-02-04 00:00:22 -0600

    Información cliente

---
+++ Models/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab0_EDI.Models
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Description { get; set; }
+
+    }
+}
